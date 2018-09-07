6a51852f39ead953c6e5bc8ba11ff71924b8b9dd
Nelson Quintanilla <nelson.quinta@hotmail.com>
2018-09-06 23:17:02 -0600

    without warnings

--- app/src/main/java/com/applaudostudios/scorekeeperappchallenge/MainActivity.java
+++ app/src/main/java/com/applaudostudios/scorekeeperappchallenge/MainActivity.java
@@ -7,25 +7,52 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     int scoreRealMadrid = 0;
-    int GoalsByPenaltyRealMadrid = 0;
-    int foulsRealMadrid = 0;
-    int yellowCardsRealMadrid = 0;
-    int redCardsRealMadrid = 0;
+    TextView realMadridScore;
+    TextView goalFromPenaltyRealMadrid;
+    TextView foulsRealMadrid;
+    TextView yellowCardsRealMadrid;
+    TextView redCardsRealMadrid;
+    TextView redCards2RealMadrid;
 
     int scoreBarcelona = 0;
-    int GoalsByPenaltyBarcelona = 0;
-    int foulsBarcelona = 0;
-    int yellowCardsBarcelona = 0;
-    int redCardsBarcelona = 0;
+    TextView barcelonaScore;
+    TextView goalFromPenaltyBarcelona;
+    TextView foulsBarcelona;
+    TextView yellowCardsBarcelona;
+    TextView redCardsBarcelona;
+    TextView redCards2Barcelona;
 
-    TextView realMadridScores;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        realMadridScores = findViewById(R.id.real_madrid_score);
-        realMadridScores.setText(String.valueOf(0));
+
+        realMadridScore = findViewById(R.id.real_madrid_score);
+        realMadridScore.setText(String.valueOf(0));
+        goalFromPenaltyRealMadrid = findViewById(R.id.goalFromPenaltyRealMadridTextView);
+        goalFromPenaltyRealMadrid.setText(String.valueOf(0));
+        foulsRealMadrid = findViewById(R.id.foulsRealMadridTextView);
+        foulsRealMadrid.setText(String.valueOf(0));
+        yellowCardsRealMadrid = findViewById(R.id.yellowCardsRealMadridTextView);
+        yellowCardsRealMadrid.setText(String.valueOf(0));
+        redCardsRealMadrid = findViewById(R.id.redCardsRealMadridTextView);
+        redCardsRealMadrid.setText(String.valueOf(0));
+        redCards2RealMadrid = findViewById(R.id.redCards2RealMadridTextView);
+        redCards2RealMadrid.setText(String.valueOf(0));
+
+        barcelonaScore = findViewById(R.id.barcelona_score);
+        barcelonaScore.setText(String.valueOf(0));
+        goalFromPenaltyBarcelona = findViewById(R.id.goalFromPenaltyBarcelonaTextView);
+        goalFromPenaltyBarcelona.setText(String.valueOf(0));
+        foulsBarcelona = findViewById(R.id.foulsBarcelonaTextView);
+        foulsBarcelona.setText(String.valueOf(0));
+        yellowCardsBarcelona = findViewById(R.id.yellowCardsBarcelonaTextView);
+        yellowCardsBarcelona.setText(String.valueOf(0));
+        redCardsBarcelona = findViewById(R.id.redCardsBarcelonaTextView);
+        redCardsBarcelona.setText(String.valueOf(0));
+        redCards2Barcelona = findViewById(R.id.redCards2BarcelonaTextView);
+        redCards2Barcelona.setText(String.valueOf(0));
     }
 
     public void addGoalForBarcelona(View v) {
